d633f6235a5c3a9ac9f9fb3e79f12b5a3202ca18
Ubuntu <ubuntu@ip-172-31-3-57.ap-southeast-1.compute.internal>
2020-02-03 05:48:28 +0000

    added

--- DiskSpace.java
+++ DiskSpace.java
@@ -5,7 +5,7 @@ public class DiskSpace
     public static void main(String[] args) 
     { 
         double size =  
-              new File("C:\\").getUsableSpace() / (1024.0 * 1024 * 1024); 
+              new File("/home/ubuntu").getUsableSpace() / (1024.0 * 1024 * 1024); 
         System.out.printf( "%.3f GB\n", size);     
     } 
 } 
